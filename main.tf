80bc4eba21a72f08df32c44b2a33c977aed87262
Peter Saldanha <Peter.Saldanha@neudesic.com>
2025-11-09 21:21:51 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-resource "azurerm_resource_group" "example" {
-  name     = "terraform-resources"
-  location = "East US"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "terraform-resources"
+#   location = "East US"
+# }
 
-
-resource "azurerm_resource_group" "example" {
-  name     = "terraform-resources_1"
-  location = "East US"
-}
